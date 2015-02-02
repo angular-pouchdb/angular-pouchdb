f3197fe6ee630eaf49cf6ed1f2973666a1fb6578
Tom Vincent <git@tlvince.com>
2015-02-02 11:41:24 +0100

    2.0.0-rc.1

--- dist/angular-pouchdb.js
+++ dist/angular-pouchdb.js
@@ -2,73 +2,89 @@
 'use strict';
 
 angular.module('pouchdb', [])
-  .constant('POUCHDB_DEFAULT_METHODS', [
-    'destroy',
-    'put',
-    'post',
-    'get',
-    'remove',
-    'bulkDocs',
-    'allDocs',
-    'sync',
-    'putAttachment',
-    'getAttachment',
-    'removeAttachment',
-    'query',
-    'viewCleanup',
-    'info',
-    'compact',
-    'revsDiff'
-  ])
-  .provider('pouchDB', ["POUCHDB_DEFAULT_METHODS", function(POUCHDB_DEFAULT_METHODS) {
-    this.methods = POUCHDB_DEFAULT_METHODS;
-    this.$get = ["$q", "$window", "$rootScope", function($q, $window, $rootScope) {
-      var methods = this.methods;
-      function qify(fn) {
-        return function() {
-          var deferred = $q.defer();
-          function callback(err, res) {
-            return $rootScope.$apply(function() {
-              if (err) {
-                return deferred.reject(err);
-              }
-              return deferred.resolve(res);
+  .constant('POUCHDB_METHODS', {
+    destroy: 'qify',
+    put: 'qify',
+    post: 'qify',
+    get: 'qify',
+    remove: 'qify',
+    bulkDocs: 'qify',
+    allDocs: 'qify',
+    putAttachment: 'qify',
+    getAttachment: 'qify',
+    removeAttachment: 'qify',
+    query: 'qify',
+    viewCleanup: 'qify',
+    info: 'qify',
+    compact: 'qify',
+    revsDiff: 'qify',
+    changes: 'eventEmitter',
+    sync: 'eventEmitter',
+    replicate: {
+      to: 'eventEmitter',
+      from: 'eventEmitter'
+    }
+  })
+  .service('pouchDecorators', ["$q", function($q) {
+    this.qify = function(fn) {
+      return function() {
+        return $q.when(fn.apply(this, arguments));
+      };
+    };
+
+    this.eventEmitter = function(fn) {
+      return function() {
+        var deferred = $q.defer();
+        var emitter = fn.apply(this, arguments)
+          .on('change', function(change) {
+            return deferred.notify({
+              change: change
             });
+          })
+          .on('uptodate', function(uptodate) {
+            return deferred.notify({
+              uptodate: uptodate
+            });
+          })
+          .on('complete', function(response) {
+            return deferred.resolve(response);
+          })
+          .on('error', function(error) {
+            return deferred.reject(error);
+          });
+        emitter.$promise = deferred.promise;
+        return emitter;
+      };
+    };
+  }])
+  .provider('pouchDB', ["POUCHDB_METHODS", function(POUCHDB_METHODS) {
+    this.methods = POUCHDB_METHODS;
+    this.$get = ["$window", "pouchDecorators", function($window, pouchDecorators) {
+      var methods = this.methods;
+
+      function wrapMethods(db, methods, parent) {
+        for (var method in methods) {
+          var wrapFunction = methods[method];
+
+          if (!angular.isString(wrapFunction)) {
+            return wrapMethods(db, wrapFunction, method);
           }
-          var args = [];
-          if (arguments !== null) {
-            args = Array.prototype.slice.call(arguments);
+
+          wrapFunction = pouchDecorators[wrapFunction];
+
+          if (!parent) {
+            db[method] = wrapFunction(db[method]);
+            continue;
           }
-          args.push(callback);
-          fn.apply(this, args);
-          return deferred.promise;
-        };
+
+          db[parent][method] = wrapFunction(db[parent][method]);
+        }
+        return db;
       }
 
       return function pouchDB(name, options) {
         var db = new $window.PouchDB(name, options);
-
-        var api = {};
-        methods.forEach(function(method) {
-          api[method] = qify(db[method].bind(db));
-        });
-
-        api.changes = function(options) {
-          var clone = angular.copy(options);
-          clone.onChange = function(change) {
-            return $rootScope.$apply(function() {
-              return options.onChange(change);
-            });
-          };
-          return db.changes(clone);
-        };
-
-        api.replicate = {
-          to: db.replicate.to.bind(db),
-          from: db.replicate.from.bind(db)
-        };
-
-        return api;
+        return wrapMethods(db, methods);
       };
     }];
   }]);
